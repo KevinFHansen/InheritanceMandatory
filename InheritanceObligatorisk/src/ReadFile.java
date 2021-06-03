60fad8bba3be48d11fd9220bd21c87daf718b9ef
KevinFHansen <kevi4777@stud.kea.dk>
2021-06-03 16:43:46 +0200

    Opgave1

--- InheritanceObligatorisk/src/ReadFile.java
+++ InheritanceObligatorisk/src/ReadFile.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class ReadFile {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File testfile = new File("src/resources/test.csv");
-        Scanner sc = new Scanner(testfile);
+        File testFile = new File("InheritanceObligatorisk/src/resources/test.csv");
+        Scanner sc = new Scanner(testFile);
 
         int counter = 0;
         while (sc.hasNext()){
